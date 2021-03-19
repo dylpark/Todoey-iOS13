e8f3d635c82df2a6946ba0617c6eefb0857f000e
Dylan Park <dylpark@protonmail.com>
2021-03-19 15:19:18 +1000

    Adding CategoryViewController

---
+++ Todoey/Controller/CategoryViewController.swift
@@ -0,0 +1,41 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Dylan Park on 19/3/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    //MARK: - TableView Datasource Methods
+    //Display all the categories inside the persistent container
+    
+    
+    
+    //MARK: - Data Manipulation Methods
+    //Save Data, Load Data
+    
+    
+    
+    //MARK: - Add New Categories
+    //Using Categories Entity
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        
+    }
+    
+    
+    //MARK: - TableView Delegate Methods
+    //What should happen when we click on one of the cells inside the CategoryTableView
+    
+}

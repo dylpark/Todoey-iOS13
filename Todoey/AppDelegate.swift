c6730cfc093d2eaa27a6dd0b4baa37643be23784
Dylan Park <dylan@Dylans-MacBook-Pro.local>
2021-03-24 20:07:34 +1000

    ...

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -11,19 +11,23 @@ import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        print(Realm.Configuration.defaultConfiguration.fileURL)
+        // print(Realm.Configuration.defaultConfiguration.fileURL)
         
         do {
-            _ = try Realm()
+            let realm = try Realm()
         } catch {
-            print("error initialising new realm, \(error)")
+            print("Error initialising new realm, \(error)")
         }
         
         return true
     }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
 }
+
